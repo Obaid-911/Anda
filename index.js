5e836b291423988b37f5d16eed5c05387d5974fb
obaid <obaidmohammed911@gmail.com>
2024-12-04 12:17:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/instagram', (req, res) =>{
+    res.send('mohammedobaid_731')
+})
+
+app.get('/cod', (req, res) => {
+    res.send('<h1>draken13</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
