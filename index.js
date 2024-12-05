5ac55d6cc9beeee160a6987104909615cdaaa810
obaid <obaidmohammed911@gmail.com>
2024-12-05 12:00:25 +0530

    add to github

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/instagram', (req, res) =>{
-    res.send('mohammedobaid_731')
+app.get('/instagram', (req, res) => {
+  res.send('mohammedobaid_731')
 })
 
 app.get('/cod', (req, res) => {
-    res.send('<h1>draken13</h1>')
+  res.send('<h1>draken13</h1>')
 })
 
-app.get('/twitter', (req, res)=> {
+// Ensure the /twitter endpoint is defined only once
+app.get('/twitter', (req, res) => {
   res.send('obaid')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
